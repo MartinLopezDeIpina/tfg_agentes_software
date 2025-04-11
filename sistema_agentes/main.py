800dabc05c6bafcf1290e28accc4a2e77b0e560e
MartinLopezDeIpina <mlopezdeipina001@ikasle.ehu.eus>
2025-04-11 12:49:18 +0200

    agente gitlab con varias tools llamando a la api

--- sistema_agentes/main.py
+++ sistema_agentes/main.py
@@ -1,7 +1,7 @@
 import asyncio
 from dotenv import load_dotenv
 
-from src.agente_react_prebuilt import ejecutar_agente_codigo, execute_confluence_agent
+from src.agente_react_prebuilt import ejecutar_agente_codigo, execute_confluence_agent, execute_gitlab_agent
 
 if __name__ == '__main__':
     load_dotenv()
@@ -9,4 +9,5 @@ if __name__ == '__main__':
     #asyncio.run(ejecutar_agente_codigo("Qué es lo que explica cada notebook de jupyter?"))
 
     #asyncio.run(execute_confluence_agent("Existe alguna guía de estilos para el proyecto?, si es así, qué color primario se utiliza?"))
-    asyncio.run(execute_confluence_agent("Qué funcionalidades tiene el frontend?"))
+    #asyncio.run(execute_confluence_agent("Qué funcionalidades tiene el frontend?"))
+    asyncio.run(execute_gitlab_agent("Qué ramas existen en el proyecto?"))
