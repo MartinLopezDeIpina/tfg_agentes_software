508608518f0c1b54f06d948bd2c14a49102f69a8
MartinLopezDeIpina <mlopezdeipina001@ikasle.ehu.eus>
2025-06-12 09:20:47 +0200

    backup

---
+++ frontend/script.sh
@@ -0,0 +1,9 @@
+docker run -d \
+  -p 3000:8080 \
+  -v open-webui:/app/backend/data \
+  -e WEBUI_AUTH=False \
+  -e OPENAI_API_BASE_URL=http://172.17.0.1:5000/api \
+  -e OPENAI_API_KEY=none \
+  -e RAG_EMBEDDING_MODEL="" \
+  --name open-webui \
+  ghcr.io/open-webui/open-webui:main
